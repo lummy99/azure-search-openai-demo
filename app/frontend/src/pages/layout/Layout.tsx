f48da3a04759caf978a8e5b148d4f98cadfc62e3
lummy99 <134435671+lummy99@users.noreply.github.com>
2023-05-30 07:28:51 -0400

    Update Layout.tsx

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -24,11 +24,11 @@ const Layout = () => {
                                    Query your Data
                                 </NavLink>
                             </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://https://www.brookfield.com/" target={"_blank"} title="Brookfield's website">
-                                    <img src="assets/logo.png"
-                                        alt="Brookfield logo"
-                                        aria-label="Link to Brookfield's website"
+                            <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                    <img
+                                        src={github}
+                                        alt="Github logo"
+                                        aria-label="Link to github repository"
                                         width="20px"
                                         height="20px"
                                         className={styles.githubLogo}
